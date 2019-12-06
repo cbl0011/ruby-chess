52ad886e8234440414b552576f113aa30d96f1be
Cody Long <codylong88@yahoo.com>
2019-12-06 01:00:59 -0600

    Block rook from moving through units

--- rook.rb
+++ rook.rb
@@ -14,8 +14,28 @@ class Rook < Piece
             col_i = current_space[1]
             row_n = next_space[0]
             col_n = next_space[1]
-            if !(row_i == row_n || col_i == col_n)
-                return false
+            if row_i == row_n 
+                if col_i > col_n 
+                    enum = (col_n + 1..col_i - 1)
+                else
+                    enum = (col_i + 1..col_n - 1)
+                end
+                enum.each do |i|
+                    if board[row_n][i]
+                        return false
+                    end
+                end
+            elsif col_i == col_n
+                if row_i > row_n 
+                    enum = (row_n + 1..row_i - 1)
+                else
+                    enum = (row_i + 1..row_n - 1)
+                end
+                enum.each do |i|
+                    if board[i][col_n]
+                        return false
+                    end
+                end
             end
             return true
         else
